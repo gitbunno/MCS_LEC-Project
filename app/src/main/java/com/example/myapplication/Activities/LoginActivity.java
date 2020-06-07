2d4cd0c0b8a51a18cf76002e2e7ae95c8bc07edb
Dragnalio <handikalimanto@gmail.com>
2020-06-07 22:20:16 +0700

    Added Login/Register Button

--- app/src/main/java/com/example/myapplication/Activities/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/LoginActivity.java
@@ -2,15 +2,45 @@ package com.example.myapplication.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button login, register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.login_btn_login);
+        login.setOnClickListener(loginListener);
+
+        register = findViewById(R.id.login_btn_register);
+        login.setOnClickListener(registerListener);
+
     }
+
+    private View.OnClickListener loginListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            };
+
+    private View.OnClickListener registerListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                }
+            };
 }
--- app/src/main/java/com/example/myapplication/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.myapplication.Activities.LoginActivity;
 import com.example.myapplication.R;
 
 
@@ -21,6 +24,8 @@ public class ProfileFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    Button btnLogin;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +66,19 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btnLogin = v.findViewById(R.id.profile_btn_login);
+
+        return v;
     }
+
+    private View.OnClickListener loginListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                    startActivity(intent);
+                }
+            };
 }
