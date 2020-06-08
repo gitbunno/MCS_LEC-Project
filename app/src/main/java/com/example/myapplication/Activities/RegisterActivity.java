0dd4cbe3f30bcbe1b165f7ae1d84554eaafc1450
Dragnalio <handikalimanto@gmail.com>
2020-06-08 18:32:03 +0700

    Cleanup

--- app/src/main/java/com/example/myapplication/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/RegisterActivity.java
@@ -2,15 +2,54 @@ package com.example.myapplication.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button btnRegister;
+    TextView btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnLogin = findViewById(R.id.register_btn_login);
+        btnLogin.setOnClickListener(loginListener);
+
+        btnRegister = findViewById(R.id.register_btn_register);
+        btnRegister.setOnClickListener(registerListener);
+    }
+
+
+    private View.OnClickListener loginListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            };
+
+    private View.OnClickListener registerListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(validate()){
+                        finish();
+                    }
+                }
+            };
+
+    private boolean validate(){
+        boolean valid = true;
+
+        return valid;
     }
 }
